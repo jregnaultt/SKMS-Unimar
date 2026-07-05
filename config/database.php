54ca1d605c4227a6be05668c4db7d3c2353e697c
Javier Regnault <jregnault.6759@unimar.edu.ve>
2026-07-05 16:15:03 -0400

    fix(deploy): disable SSL CA verification for Aiven MySQL connection

--- config/database.php
+++ config/database.php
@@ -59,7 +59,10 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', true),
+            ], function ($value) {
+                return $value !== null && $value !== '';
+            }) : [],
         ],
 
         'mariadb' => [
